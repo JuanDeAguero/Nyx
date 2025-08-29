4c6e9c0351abfe032036e8fe84c0cd92dc936750
Juan <juanmgdeaguero@gmail.com>
2025-08-29 13:00:20 +0200

    Swap chain

--- main.cpp
+++ main.cpp
@@ -1,37 +1,33 @@
 #include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
 
-LRESULT CALLBACK WindowProc(
-    HWND hwnd,
-    UINT uMsg,
-    WPARAM wParam,
-    LPARAM lParam
-) {
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
     }
+
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-int WINAPI WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE,
-    LPSTR,
-    int nCmdShow
-) {
-    const wchar_t CLASS_NAME[] = L"DX12WindowClass";
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const wchar_t className[] = L"DX12WindowClass";
 
     WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+    wc.lpszClassName = className;
 
     RegisterClass(&wc);
-
+    
     HWND hwnd = CreateWindowEx(
         0,
-        CLASS_NAME,
+        className,
         L"Nyx",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT,
@@ -44,9 +40,48 @@ int WINAPI WinMain(
         nullptr
     );
 
-    if (!hwnd) return 0;
+    if (!hwnd) return -1;
 
     ShowWindow(hwnd, nCmdShow);
+    
+    Microsoft::WRL::ComPtr<ID3D12Device> device;
+    HRESULT hr = D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
+    if (FAILED(hr)) return -1;
+
+    Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    hr = device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&commandQueue));
+    if (FAILED(hr)) return -1;
+
+    Microsoft::WRL::ComPtr<IDXGIFactory6> dxgiFactory;
+    hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+    if (FAILED(hr)) return -1;
+
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    swapChainDesc.BufferCount = 2;
+    swapChainDesc.Width = 800;
+    swapChainDesc.Height = 600;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.SampleDesc.Count = 1;
+
+    Microsoft::WRL::ComPtr<IDXGISwapChain1> swapChain1;
+    hr = dxgiFactory->CreateSwapChainForHwnd(
+        commandQueue.Get(),
+        hwnd,
+        &swapChainDesc,
+        nullptr,
+        nullptr,
+        &swapChain1
+    );
+    if (FAILED(hr)) return -1;
+
+    Microsoft::WRL::ComPtr<IDXGISwapChain3> swapChain;
+    hr = swapChain1.As(&swapChain);
+    if (FAILED(hr)) return -1;
 
     MSG msg = {};
     while (msg.message != WM_QUIT) {
